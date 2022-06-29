db111142bac2ff6b3c81408ed3c40d0c0f15bba7
Aaron McEvoy <90684015+Amacyy@users.noreply.github.com>
2022-06-29 12:08:11 +0100

    Add files via upload

---
+++ InClass230921/src/GetOffset.java
@@ -0,0 +1,93 @@
+public class GetOffset {
+    public static int findIndex(char [] ca, char c)
+    {
+        for(int i = 0; i < ca.length; i++)
+        {
+            if(ca[i] == c)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public String toString()
+    {
+        return "nothing";
+    }
+    public static int [] getOffset(char [] first, char [] second)
+    {
+        int [] returnValue = {-1,-1};
+
+
+        for(int i = 0; i < first.length; i++)
+        {
+            int index = findIndex(second, first[i]);
+
+            if (index != -1)
+            {
+                returnValue[0] = i;
+                returnValue[1] = index;
+                return returnValue;
+            }
+        }
+
+        return returnValue;
+    }
+
+    public static void main(String[] args) {
+        String s ="hello";
+        String w = "world";
+
+        char [][]ca = new char[10][10];
+
+        int [] returned =
+                getOffset(s.toCharArray(), w.toCharArray());
+
+        //char [][] ca = new char[10][10];
+
+        for(int i = 0;  i < s.length(); i++)
+        {
+            ca[returned[1]][i] = s.charAt(i);
+        }
+
+        for(int i = 0;  i < w.length(); i++)
+        {
+            ca[i][returned[0]] = w.charAt(i);
+        }
+
+        for(int i = 0; i < ca.length;i++)
+        {
+            for (int j = 0; j < ca[i].length; j++)
+            {
+                System.out.print(ca[i][j]);
+            }
+
+            System.out.println();
+        }
+
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            ca[returned[1]][i] = s.charAt(i);
+        }
+
+        for(int i = 0; i < w.length(); i++)
+        {
+            ca[i][returned[0]] = w.charAt(i);
+        }
+
+
+
+        for(int i = 0; i < ca.length; i++)
+        {
+            for (int j = 0; j < ca[i].length; j++)
+            {
+                System.out.print(ca[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# Java-Projects-Year-2
+# README #
+
+All code from class and weekly labs will be uploaded to this repository
+
+## Lab Exam Solutions
+[Solutions Folder](LabExamSolution) 
+
+## Code Directory 
+[InClass010921](InClass010921)  - lab1 tutorial
+
+[InClass060921](InClass060921)  - lab2 tutorial and class object demo
+
+[InClass160921](InClass160921)  - Recursion Demo
+
+[InClass230921](InClass230921)  - lab2 getOffset demo, File reading demo
+
+[InClass260921](InClass260921)  - lab3 Jumble words skeleton
+
+[InClass270921](InClass270921)  - Inheritance demo
+
+[InClass300921](InClass300921)  - Using Random Class and seed, Appending to text file
+
+[InClass041021](InClass041021)  - lab4 tutorial
+
+[InClass041021](InClass041021)  - File splitter fifth word 2 ways
+
+[InClass111021](InClass111021)  - print 5th word and create files demo and Loops
+
+[InClass181021](InClass181021)  - Sorting lab 5 and Validation
+
+[InClass011121](InClass011121)  - Temp Convert, Layouts, Interfaces
+
+[InClass031121](InClass031121)  - Write to file on close
+
+[InClass081121](InClass081121)  - More Sorting
+
+[InClass151121](InClass151121)  - Graphics Demo
+
+[LabExamSolution](LabExamSolution) - LabExamSolution
+
+[InClass110122](InClass110122)  - Singleton and MVC Demo, template for Lab 7
+
+[InClass180122](InClass180122)  - Scatter and WordArt
+
+[JSoupTutorial](JSoupTutorial) - JSoup (video to follow)
+
+[InClass250122](InClass250122) - Interface Abstraction, Keyboard Controller
+
+[InClass010222](InClass010222) - Shape Click, Move and Currency converter
+
+[InClass150222](InClass150222) - Collision Detection
+
+[InClass010322](InClass010322) - Hashtable and Threads Demo
+
+
+### Tutorials
+[Tutorials](Tutorials) - All Tutorials will be in this folder
+- Clicking Colors
+- Lab 8 Starter Template
+- Using the layout manager for JFrame
+- Dropping shapes
+- Arraylist of Abstract Shape 
+- Basics part 1
+- JSoupGUI
+- Demo Timer
+
+### Exam Prep
+[ExamPrep](ExamPrep) - Exam Tutorials
